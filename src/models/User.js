2eb3834fe35650aaf04e98cac14e51c7fdab27d5
alenprastyaa <1001cerita1001@gmail.com>
2025-12-05 12:36:27 +0700

    first commit

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const sequelize = require("../config/database");
 const bcrypt = require("bcryptjs");
 
 const User = sequelize.define(
-  "Users",
+  "users",
   {
     id: {
       type: DataTypes.UUID,
